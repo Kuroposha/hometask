1c8be7f133bac4df04839cdc08dfacbf68eca529
Kuroposha <xanymahot@gmail.com>
2018-01-28 16:47:12 +0300

    Домашняя рабоата 1

---
+++ task_01_02.py
@@ -0,0 +1,18 @@
+"""
+Домашняя работа
+1.2 Тарелки
+"""
+PLATES = int(input())
+SOAP = int(input())
+
+for DISHES in range(PLATES):
+    PLATES -= 1
+    SOAP -= 0.5
+
+    if PLATES > 0 and SOAP == 0:
+        print('Моющее средство закончилось. Осталось ', PLATES, ' тарелок')
+        break
+    elif PLATES == 0 and SOAP > 0:
+        print('Все тарелки вымыты. Осталось ', SOAP, ' ед. моющего средства')
+    elif PLATES == 0 and SOAP == 0:
+        print('Все тарелки вымыты, моющее средство закончилось')
