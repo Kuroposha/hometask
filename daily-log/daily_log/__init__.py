f3176ac8b091a6314d1fe10d5e6f22ea66adcf78
Kuroposha <xanymahot@gmail.com>
2018-02-11 15:51:34 +0300

    it work but ugly

--- daily-log/daily_log/__init__.py
+++ daily-log/daily_log/__init__.py
@@ -1,62 +1,154 @@
 import sys
 
-def show_menu():
-    print('''\nЕжедневник.\nГлавное меню. Выберете действие:\n
-    1. Вывести список задач\n
-    2. Добавить задачу\n
-    3. Отредактировать задачу\n
-    4. Завершить задачу\n
-    5. Начать задачу сначала\n
-    6. Выход''')
+from daily_log import storage
 
 
-def task_list():
-    print('1. Вывести список задач')
-    # print('\nСписок текущих задач:\n')
-    # return
+get_connection = lambda: storage.connect('daily_task.sqlite')
 
 
-def add_task():
-    print('2. Добавить задачу')
-    #return print('\nВы можете добавить новую задачу:\n')
+def action_show_menu():
+    """Вывод меню"""
+    print(
+    '''
+    0. Выход
+    1. Показать меню
+    2. Вывести список задач
+    3. Добавить задачу
+    4. Отредактировать задачу
+    5. Завершить задачу
+    6. Начать задачу сначала
+    '''
+    )
 
 
-def edit_task():
-    print('3. Отредактировать задачу')
-    #return print('\nВы можете отредактировать одну из созданных задач:\n')
+def action_add_t():# УРА! это дерьмо наконец то заработало!
+    """Добавить задачу"""
+    name_task = input('\nВведите название задачи: ')
+
+    answ = input("\nВы хотите ввести описание поставленной задачи? (да/ нет)")
+    if answ == 'да':
+        description_t = input("\nВведите описание задачи: ")
+    else:
+        description_t = name_task
+    with get_connection() as conn:
+        name = storage.add_task(conn, name_task, description_t)
+    print('\nНовая задача добавлена в ежедневник!')
 
 
-def close_task():
-    print('4. Завершить задачу')
-    #return print('\nВы можете завершить одну из текущих задач:\n')
+def action_show_tl():# УРА! это дерьмо наконец то заработало!
+    """Вывести список задач"""
+    with get_connection() as conn:
+        all_tasks = storage.find_all(conn)
+    id_t = 1
+    for id_t in all_tasks:
+        visual = '{id_t[id]} - {id_t[name]} - {id_t[description]} - {id_t[plan_date]} - {id_t[status]}'
+        print(visual.format(id_t=id_t))
 
 
-def restart_task():
-    print('5. Начать задачу сначала')
-    #return print('\nВы можете начать одну из предыдущих задач заново:\n')
+def action_edit_t():# УРА! это дерьмо наконец то заработало!
+    """Отредактировать задачу"""
+    """Идея в том, что редактировать закртые задачи нельзя! Поэтому нужна выборка по открытым
+    задачам. Редактировать в них можно название, описание и дату на любую, кроме прошлого
+    нужно предложить пользователю список открытых задач, или сразу ввести название или номер"""
+    answ = input('\nВы знаете номер задачи, которую хотите отредактировать? да/ нет: ')
+    if answ == 'да':
+        id_t = int(input('\nВведите номер задачи для редактирования: '))
+    else:
+        print('Выберете задачу из списка:\n')
+        with get_connection() as conn:
+            all_open_tasks = storage.find_for_status_task(conn, 0)
+
+        for id_t in all_open_tasks:
+            visual = '{id_t[id]} - {id_t[name]} - {id_t[description]}'
+            print(visual.format(id_t=id_t))
+
+        id_t = input('\nВведите номер задачи для редактирования: ')
+    name_task = input('\nВведите новое имя задачи: ')
+    description_t = input('\nВведите новое описание задачи: ')
+
+    with get_connection() as conn:
+        name = storage.change_task_options(conn, name_task, description_t, id_t)
 
 
-def exit_work():
-    print('6. Выход')
-    sys.exit()
-    #return print('\nСпасибо за работу. До новых встреч!\n')
+def action_replay_t():# УРА! это дерьмо наконец то заработало!
+    """Начать задачу заново"""
+    """Идея в том, что нельзя начать заново то. что еще не закончено. И пока я писала предыдущие тексты
+    я решила, что нет смысла делать отбор по имени и пошла менять все, что есть на айди
+    также список или номер"""
+    answ = input('\nВы знаете номер задачи, которую хотите начать заново? да/ нет: ')
+    if answ == 'да':
+        id_t = int(input('\nВведите номер задачи для перезапуска: '))
+    else:
+        print('Выберете задачу из списка:\n')
+        with get_connection() as conn:
+            all_close_tasks = storage.find_for_status_task(conn, 1)
+"""когда нибудь я вынесу это в отдельную функцию, но пока работает"""
+        for id_t in all_close_tasks:
+            visual = '{id_t[id]} - {id_t[name]} - {id_t[description]}'
+            print(visual.format(id_t=id_t))
+
+        id_t = int(input('\nВведите номер задачи для перезапуска: '))
+
+    with get_connection() as conn:
+        status_t = storage.change_status_task(conn, 0, id_t)
 
 
-def menu_daily_log():
-    dict_of_menu = {'1': task_list,
-                    '2': add_task,
-                    '3': edit_task,
-                    '4': close_task,
-                    '5': restart_task,
-                    '6': exit_work
-                    }
-    show_menu()
 
-    while True:
-        num_act = input('\nВведите номер меню:\n')
-        func = dict_of_menu.get(num_act)
+def action_close_t():# УРА! это дерьмо наконец то заработало!
+    """Завершить задачу"""
 
-        if func:
-            func()
-        else:
-            print('Номер меню задан неверно')
+    """Идея в том, что завершить можно только открытую задачу. Поэтому нужна выборка по
+    открытым задачам и одновременно с этим по имени или айди задачи. НУжно предложить пользователю Вывести
+    айди, имя или список открытых задач."""
+    answ = input('\nВы знаете номер задачи, которую хотите завершить? да/ нет: ')
+    if answ == 'да':
+        id_t = int(input('\nВведите номер задачи для завершения: '))
+    else:
+        print('Выберете задачу из списка:\n')
+        with get_connection() as conn:
+            all_open_tasks = storage.find_for_status_task(conn, 0)
+
+        for id_t in all_open_tasks:
+            visual = '{id_t[id]} - {id_t[name]} - {id_t[description]}'
+            print(visual.format(id_t=id_t))
+
+        id_t = int(input('\nВведите номер задачи для завершения: '))
+
+    with get_connection() as conn:
+        status_t = storage.change_status_task(conn, 1, id_t)
+
+
+def action_exit():
+    """Завершить работу программы"""
+    print("Спасибо за работу. До новых встреч!")
+    sys.exit(0)
+
+def main_dl():
+    """Запуск программы ежедневника"""
+    print('''
+    Ежедневник.
+    Главное меню.
+    ''')
+    with get_connection() as conn:
+        storage.initualize(conn)
+
+        actions = {
+            '0': action_exit,
+            '1': action_show_menu,
+            '2': action_show_tl,
+            '3': action_add_t,
+            '4': action_edit_t,
+            '5': action_close_t,
+            '6': action_replay_t,
+        }
+
+        action_show_menu()
+
+        while True:
+            num_act = input('\nВведите номер меню: \n')
+            func = actions.get(num_act)
+
+            if func:
+                func()
+            else:
+                print('Номер меню задан неверно')
--- daily-log/daily_log/storage.py
+++ daily-log/daily_log/storage.py
@@ -1,18 +1,117 @@
 import os.path as Path
 import sqlite3
 
+# запрос на добавление строки в таблицу
+# запрос вывод списка текущих задач
+# ЗАпрос на редакцию задачи (только невыполненные)
+# запрос на завершение задачи (только невыполненные)
+# запрос на перезапуск задачи  - (только выполненные с изменением даты)
+# Селекты - все, все выполненные, все невыполненные, с айди
+SQL_SELECT_ALL = '''
+    SELECT
+        id, name, description, plan_date, status
+    FROM
+        daily_task
+'''
 
-sql_dir = Path.join(Path.dirname(__file__), 'resourses', 'schema.sql')
+SQL_SELECT_ID = SQL_SELECT_ALL + ' WHERE id=?'
 
-with sqlite3.connect(':memory:') as conn, open(sql_dir) as f:
+SQL_SELECT_STATUS = SQL_SELECT_ALL + ' WHERE status=?'
 
-    conn.executescript(f.read())
+SQL_INSERT_TASK = '''
+    INSERT INTO daily_task (name, description) VALUES (?, ?)
+'''
+
+SQL_UPDATE_TASK_STATUS = '''
+    UPDATE daily_task SET status=? WHERE id=?
+'''
+
+SQL_UPDATE_ALL_USER_WANT = '''
+    UPDATE daily_task SET name=?, description=? WHERE id=?
+'''
 
 
+def dict_factory(cursor, row):# УРА! это дерьмо наконец то заработало!
+    """функция, магическим образом помогающая превращать нашу таблицу в словарь для красоты"""
+    d = {}
 
-    # conn.execute(sql_dir)
-    # try:
-    #     coursor = conn.execute(sql_dir)
-    #     print(cursor.fetchall())
-    # except sqlite3.Error as e:
-    #     print(e)
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+
+    return d
+
+
+def connect(db_name=None):
+    """Установить соединение с БД"""# УРА! это дерьмо наконец то заработало!
+    if db_name is None:
+        db_name = ':memory:'
+
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = dict_factory
+
+    return conn
+
+
+def initualize(conn, creation_script=None):
+    """Инициализировать структуру БД"""# УРА! это дерьмо наконец то заработало!
+    if creation_script is None:
+        creation_script = Path.join(Path.dirname(__file__), 'resourses', 'schema.sql')
+
+    with conn, open(creation_script) as f:
+        conn.executescript(f.read())
+
+
+def add_task(conn, name_task, description_t):
+    """Добавляет задачу в БД"""# УРА! это дерьмо наконец то заработало!
+
+    if not name_task:
+        raise RuntimeError('Не указано название задачи!')
+
+    with conn:
+        cursor = conn.execute(SQL_INSERT_TASK, (name_task, description_t,))
+
+
+# def find_task(conn, id_t):
+#
+#     with conn:
+#         cursor = conn.execute(SQL_SELECT_ID, (id_t,))
+#         return cursor.fetchone()
+
+def find_all(conn):# УРА! это дерьмо наконец то заработало!
+    """Найти все задачи"""
+    with conn:
+        cursor = conn.execute(SQL_SELECT_ALL)
+        return cursor.fetchall()
+
+
+def find_for_status_task(conn, status_t):
+     """Ищем задачи по статусу 0\1"""
+     with conn:
+         cursor = conn.execute(SQL_SELECT_STATUS, (status_t,))
+         return cursor.fetchall()
+
+
+def change_status_task(conn, status_t, id_t):# УРА! это дерьмо наконец то заработало!
+    """Тщетно пытаюсь реализовать инверсию статуса задачи"""
+    if not id_t:
+        raise RuntimeError('Такой задачи в списке нет!')
+
+    with conn:
+        cursor = conn.execute(SQL_UPDATE_TASK_STATUS, (status_t, id_t,))
+
+
+def change_task_options(conn, name_task, description_t, id_t):
+    """Пытаюсь прилепить редакцию задач в Ежедневник"""
+    if not id_t:
+        raise RuntimeError('Такой задачи в списке нет!')
+
+    with conn:
+        cursor = conn.execute(SQL_UPDATE_ALL_USER_WANT, (name_task, description_t, id_t,))
+
+# def find_open_task(conn, name_task):
+#     """Перезапуск задачи"""
+#     name_task =
+#     with conn:
+#         cursor = con.execute (SQL_SELECT_CLOSE, (name_task))
+#         # меняем статус на 0
+#         return
