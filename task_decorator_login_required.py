454d737d09efe8ce0bfbfcdab5959adb91d73923
Maria K <35758685+Kuroposha@users.noreply.github.com>
2018-02-15 17:25:49 +0300

    Create task_decorator_login_required.py
    Черновой вариант декоратора с паролем

---
+++ task_decorator_login_required.py
@@ -0,0 +1,29 @@
+"""
+Домашняя работа
+Задание 3 декоратор с паролем
+"""
+import hashlib
+
+
+def make_token(username, password):
+    s = username + password
+    return hashlib.md5(s.encode()).hexdigest()
+
+
+def try_to_pass(try_it):
+    with open('token.txt') as f:
+        iniz = f.read()
+    return try_it == iniz
+
+
+def login_required(func):
+    def wrapper(*args, **kwargs):
+        for i in range(3):
+            user_check = input()
+            pass_check = input()
+            it_make = make_token(try_to_pass(user_check, pass_check))
+            if it_make:
+                break
+
+        return func(*args, **kwargs) if it_make else None
+    return wrapper
