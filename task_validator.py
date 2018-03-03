6a7e4abf1a5c030fde2e4560804ff75982bab387
Kuroposha <xanymahot@gmail.com>
2018-03-03 17:44:54 +0300

    Валидатор готов

--- task_validator.py
+++ task_validator.py
@@ -3,45 +3,76 @@
 Задача. Валидатор входных данных
 """
 from abc import ABCMeta, abstractmethod
+from datetime import datetime
 
-class ValidatorException(BaseException):
+class ValidatorException(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
 
-        
+
 class Validator(metaclass=ABCMeta):
+    types = {}
+
     def __init__(self, value):
         self.value = value
 
     @abstractmethod
-    def validate(value):
-        return True if value==value
+    def validate(self, value):
+        pass
+        # return True if value==value
 
     @classmethod
-    def get_instance(name, *args, **kwargs):
+    def add_type(cls, name, klass):
+        if not name:
+            raise ValidatorException('Validator must have a name!')
+        elif not issubclass(klass, Validator):
+            raise ValidatorException(
+            'Class "{}" is not Validator!'.format(klass)
+            )
+        cls.types[name] = klass
 
-        _, ext = os.path.splitext(str(source).lower())
-        ext = ext.lstrip('.')
-        klass = cls.types.get(ext)
+    @classmethod
+    def get_instance(cls, name):
+
+        klass = cls.types.get(name)
 
         if klass is None:
             raise ValidatorException(
-                'Validator with name "{}" not found'.format(ext)
+                'Validator with name "{}" not found'.format(name)
             )
 
-        return klass(name, *args, **kwargs)
+        return klass()
 
 class EMailValidator(Validator):
     def __init__(self, value):
         super().__init__(value)
 
-    def validate(value):
-        pass
+    def validate(self, value):
+        return '@' and '.' in value
 
 
 class DateTimeValidator(Validator):
     def __init__(self, value):
         super().__init__(value)
 
-    def validate(value):
-        pass
+    def validate(self, value):
+        for key in ['%Y-%m-%d',
+                    '%Y-%m-%d %H:%M',
+                    '%Y-%m-%d %H:%M:%S',
+                    '%d.%m.%Y',
+                    '%d.%m.%Y %H:%M',
+                    '%d.%m.%Y %H:%M:%S',
+                    '%d/%m/%Y',
+                    '%d/%m/%Y %H:%M',
+                    '%d/%m/%Y %H:%M:%S'
+                    ]:
+            try:
+                if datetime.strptime(value, key):
+                    return True
+            except ValueError:
+                pass
+
+        return False
+
+Validator.add_type('email', EMailValidator)
+Validator.add_type('datetime', DateTimeValidator)
