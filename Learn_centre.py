644bed8de673f6c44820b5b6d2e8ecbfe049ffb5
Kuroposha <xanymahot@gmail.com>
2018-03-05 07:16:26 +0300

    наброски уч центра

---
+++ Learn_centre.py
@@ -0,0 +1,54 @@
+"""
+Домашка на понедельник (26.02.18)
+
+Задача №1. Учебный центр
+
+Перенести в ОО-код следующий пример из реального мира:
+- есть курсы, учителя и ученики
+- у каждого курса есть свой учитель
+- у каждого учителя есть своя группа учеников
+
+Определите какие объекты есть в этом примере, какие у них
+свойства и какие методы, как эти объекты будут между собой
+взаимодействовать, например, к курсу можно добавить учителя.
+
+Создайте все необходимые классы и приведите пример их
+использования.
+
+"""
+
+class Course(object):
+    """ """
+    def __init__(self, name, date_start, duration, location, coast, programm):
+        """ """
+
+    def set_education():
+        """ """
+
+    def set_teach():
+
+    def get_
+
+
+class Teacher(object):
+    """ """
+    def __init__(self, name, age, exp, **specialisation):
+        """ """
+
+    def get_teaching():
+
+    def setting_hometask():
+
+    def presintation():
+
+    def reading_stubif_esse():
+
+    def det_mark():
+
+class Group(object):
+    """ """
+    def __init__(self, name, students_num):
+
+
+class Student(object):
+    """ """
